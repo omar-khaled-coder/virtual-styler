ece1cf78705bda60152e1b076c0b652a44a38709
omar abdrbuo <omarkh3010@gmail.com>
2023-03-06 17:26:12 +0800

    validation done

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   has_many :cart_items
   has_many :preset_items
   belongs_to :preset, through: :preset_items
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :description, presence: true, length: { maximum: 1000 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :first_name, presence: true, length: { maximum: 20 }
+  validates :last_name, presence: true, length: { maximum: 50 }
 end
