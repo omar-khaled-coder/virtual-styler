23d44c743366ab7ca22ff7b8685d12a82d25c053
ante andrijasevic <ante.adc@gmail.com>
2023-03-06 16:49:40 +0800

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
+
+# Path: app/controllers/application_controller.rb
