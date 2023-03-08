5ad8d8cfe7842cef18804e0b65061e9f39eab2c2
Robert Safin <robertsafin98@gmail.com>
2023-03-08 14:39:47 +0800

    seed for bedroom and living room

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,10 @@
 class Item < ApplicationRecord
   has_many :cart_items
   has_many :preset_items
-  belongs_to :preset, through: :preset_items
+  #belongs_to :preset, through: :preset_items
   validates :name, presence: true, length: { maximum: 50 }
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, presence: true, length: { maximum: 1000 }
+
+  has_one_attached :photo
 end
