4c1337837863faddf57dcd630550391cbf631b29
Robert Safin <robertsafin98@gmail.com>
2023-03-09 21:08:35 +0800

    added logic for basket page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,8 +11,26 @@ class PagesController < ApplicationController
 
   # contains items added by the user
   def basket
-
+    if current_user.cart == nil
+      @cart = Cart.create(user_id: current_user.id)
+    else
+      @cart = current_user.cart
+      cart_items = []
+        params.delete(:controller)
+        params.delete(:action)
+        params.each do | key, value|
+          cart_items << CartItem.create!(cart_id: @cart.id, quantity: value.to_i, item_id: Item.find(key.to_i).id)
+        end
+        @cart_items = cart_items.reject {|cart_item| cart_item.quantity === 0}
+    end
   end
+
+
+
+
+
+
+
 
   def payment
 
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,4 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :item
-  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
 end
